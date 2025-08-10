be497937afc54148bbc94ef49ca3a8aa98a9c366
Mateus Urtassum <mateusurtassum@gmail.com>
2025-08-10 16:35:21 -0300

    feat(articles): display articles list and load more functionality in ArticleByCategory
    - Added <ul> with v-for to render fetched articles by category
    - Implemented getArticles() method using Axios to fetch paginated articles from API
    - Appends new articles to existing list instead of replacing
    - Added page counter and loadMore flag to handle pagination
    - Display "Load More Articles" button, hidden when no more articles are available
    - Styled articles list (no bullets, zero padding) and centered load-more button section
    - Mounted hook now loads both category details and first page of articles

--- frontend/src/components/article/ArticleByCategory.vue
+++ frontend/src/components/article/ArticleByCategory.vue
@@ -2,6 +2,17 @@
     <div class="articles-by-categories">
         <PageTitle icon="far fa-folder"
             :main="category.name" sub="Category"/>
+
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                {{ article.name }}
+            </li>
+        </ul>
+        <div class="load-more">
+            <button v-if="loadMore" 
+            class="btn btn-lg btn-outline-primary"
+            @click="getArticles">Load More Articles</button>
+        </div>
     </div>
 </template>
 
@@ -23,18 +34,40 @@ export default {
     },
     methods:{
         getCategory(){
-            const url = `${baseApiUrl}/categories/${this.category.id}`
+            const url = `${baseApiUrl}/categories/${this.category.id}`;
             axios(url).then(res => this.category = res.data);
+        },
+        getArticles(){
+            const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`;
+            axios(url).then(res => {
+                this.articles = this.articles.concat(res.data);
+                this.page++;
+
+                if(res.data.length === 0){ //there is no data (article)
+                    this.loadMore = false;
+                } 
+            });
         }
     },
     mounted(){
         // console.log(this.$route.params.id); //id from url
         this.category.id = this.$route.params.id;
         this.getCategory();
+        this.getArticles();
     }
 }
 </script>
 
 <style>
+    .articles-by-categories ul{
+        list-style-type: none;
+        padding: 0px;
+    }
 
+    .articles-by-categories .load-more{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 25px;
+    }
 </style>
