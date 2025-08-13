3252c2c4acc819b7037e59a3d7c92d7e41197c79
Mateus Urtassum <mateusurtassum@gmail.com>
2025-08-12 21:56:32 -0300

    Insert tree (fix it later)

--- frontend/src/components/template/compMenu.vue
+++ frontend/src/components/template/compMenu.vue
@@ -1,25 +1,100 @@
 <template>
-	<aside class="menu" v-show="isMenuVisible">
-		
-	</aside>
+    <aside class="menu" v-show="isMenuVisible">
+    <p>Menu visível? </p> <!-- Temporário -->
+        <Tree :nodes="treeData" :config="treeOptions" :key="'main-tree'" ref="tree"/>
+    </aside>
 </template>
 
 <script>
-import {mapState} from 'vuex' //responsible to map atribute from store 
+import { mapState } from 'vuex';
+import Tree from 'vue3-tree';
+import { baseApiUrl } from '@/global';
+import axios from 'axios';
 
 export default {
-	name: 'mainMenu',
-    computed: mapState(['isMenuVisible'])
-}
+    name: 'mainMenu',
+    components: { Tree },
+    computed: mapState(['isMenuVisible']),
+    data() {
+        return {
+            treeData: [],
+            treeOptions: {
+                labelField: 'name',
+                keyField: 'id',
+            }
+        };
+    },
+
+    mounted() {
+        this.getTreeData();
+    },
+    methods: {
+        async getTreeData() {
+            try {
+                const url = `${baseApiUrl}/categories/tree`;
+                const res = await axios.get(url);
+                console.log('Dados recebidos:', res.data);
+                this.treeData = res.data;
+            } catch (err) {
+                console.error('Erro ao carregar dados da árvore:', err);
+            }
+        },
+    }
+};
 </script>
 
 <style>
-    .menu{
+    .menu {
         grid-area: menu;
         background: linear-gradient(to right, #232526, #414345);
 
         display: flex;
         flex-direction: column;
-        flex-wrap: wrap; /*break line */
+        flex-wrap: wrap; /*break line*/
     }
-</style>
+
+    .menu a,
+    .menu a:hover {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .menu .tree-node.selected > .tree-content,
+    .menu .tree-node .tree-content:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    .tree-arrow.has-child {
+        filter: brightness(2);
+    }
+
+    .menu .menu-filter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin: 20px;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #AAA;
+    }
+
+    .menu .menu-filter i {
+        color: #AAA;
+        margin-right: 10px;
+    }
+
+    .menu input {
+        color: #CCC;
+        font-size: 1.3rem;
+        border: 0;
+        outline: 0;
+        width: 100%;
+        background: transparent;
+    }
+
+    .tree-filter-empty {
+        color: #CCC;
+        font-size: 1.3rem;
+        margin-left: 20px;
+    }
+</style> 
